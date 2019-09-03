44b1d2f10c78796c9362c0afcbdbb31d7ae66f1a
Saroj Mandal <saroj.mandal1989@gmail.com>
2019-09-03 22:52:26 +0530

    Docker Training
    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <h1>read deployment from feature/test-aws branch</h1>
+          Hello there, this is cool bro! I am trying to upload this to aws. I am changing again in feature branch. made changes to t2.small.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
